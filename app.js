c13e978b89cd7233960ea7ef3b20f563d132030e
hisham180 <hisham@sarvacharya.com>
2021-03-27 19:24:37 +0530

    Procfile changes

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ _.each(routesList, (router, key) => {
   app.use(router.allowedMethods());
 });
 
-let server = app.listen(config.Procfile.port, () => {
+let server = app.listen(Procfile.port, () => {
   logger.info(
     `[Started] Application started listening on port ${config.application.port}`
   );
